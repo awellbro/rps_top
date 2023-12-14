d97c2bafe5d2f345455d98a93d4232adb830d4de
Aaron <wellbrockaaron@gmail.com>
2023-12-13 21:23:53 -0800

    added js random computer selection and prompt for player input. began function to play game

--- javascript.js
+++ javascript.js
@@ -1,1 +1,26 @@
-console.log("hello world!")
+const playerSelection = prompt("Choose: Rock, Paper, or Scissors");
+const computerSelection = getComputerChoice();
+
+function getComputerChoice() {
+    let num = (Math.floor(Math.random()*100))
+    if (num <= 33){
+        return "Rock";
+    }
+    else if (num <= 66){
+        return "Paper";
+    }
+    else {
+        return "Scissors";
+    };
+}
+
+
+function play(x, y) {
+ let playSel = x.toLowerCase();
+ let compSel = y.toLowerCase();
+ console.log(playSel, compSel);
+};
+
+play(playerSelection, computerSelection);
+
+//document.write(playerSelection, computerSelection);
