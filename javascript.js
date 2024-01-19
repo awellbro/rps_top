9d3eb252698eec2b90e010fe9b11de592c65de22
Aaron <wellbrockaaron@gmail.com>
2024-01-19 00:22:33 -0800

    everything works. needs styling. Current work around to restart is remove buttons and refresh to replay - needs replay function to reset score and results

--- javascript.js
+++ javascript.js
@@ -52,41 +52,23 @@ function keepCount(x){
         } else {
             if (playScore.length > compScore.length){
                 scoreDiv.textContent = ("You win!");
-                replay();
+                offButt();
              } else {
                 scoreDiv.textContent = ("You lose!");
-                replay();
+                offButt()
              }
         };
 };
 
-function replay(){
+function offButt(){
+   const removeRock = document.getElementById('rock');
+   const removePaper = document.getElementById('paper');
+   const removeScissors = document.getElementById('scissors');
 
-}
-
-/*
-    if (x.includes('Win')){
-        playScore.push("I");
-    } else {
-        compScore.push("I");
-    }
-
-    console.log(`player: ${playScore}`);
-    console.log(`comp: ${compScore}`);
-
-
-const total = document.querySelectorAll("li");
-const totalArr = Array.from(total);
-const totalText = totalArr.textContent;
-console.log(totalText);
-
-/*
-if(total.length == 5){
-    console.log(Array.from(total))
-    //document.querySelector('div').textContent = "Done";
-}
-*/
-
+   removeRock.remove();
+   removePaper.remove();
+   removeScissors.remove();
+};
 //----------------------------------------------------------------------------------------------------------
 //prompt user input - input is passed to playerSelection()
 //function playerSelection() {return prompt("Choose: Rock, Paper, or Scissors");}
@@ -107,12 +89,8 @@ function getComputerChoice() {
 
 function play(x, y) {
 
-//normalizes all responses to lower case
 let playSel = x;
 let compSel = y;
-
-//if playSel is a valid response, tests for winner
-//if (playSel === "rock" || playSel === "paper" || playSel === "scissors"){
 
     //if player and computer selections are the same, reprompts for input from both users.
     if (playSel == compSel){
@@ -131,46 +109,4 @@ let compSel = y;
         } else if(playSel == "scissors" && compSel == "rock"){
             return `You Lose! ${y} beats ${x}!`;
             }
-}
-//if player input is not a valid response, re-prompts for selection from both users
-//else {
- //   return play(playerSelection(), getComputerChoice());
- //   }
-//};
-/*
-function game(a) {
-
-    //create arrays to track scores of each player sepeartely
-    const playScore = [];
-    const compScore = [];
-
-    //checks return value strings - all strings containing "Win" push a I tally to player score
-    for(let i = 0; i < 5; i++){
-        let playRes = a;
-        if(playRes.includes("Win")){
-            playScore.push("I")
-        } else {
-            compScore.push("I")
-        }
-        //turns each array to a string
-        const playTxt = playScore.toString("");
-        const compTxt = compScore.toString("");
-
-        //prints return value from play()
-        console.log(playRes);
-
-        //prints stringified score array from this function (game())
-        console.log(`Player Score: ${playTxt} | Computer Score: ${compTxt}`);
-
-    }
-
-    //checks for the longer array length (higher score) after the game is played 5 times. prints result to console.
-    if (playScore.length > compScore.length){
-        console.log("You win!");
-     } else {
-        console.log("You lose!");
-     }
-    };
-
-//(game())();
-*/
+};
